eebaa1e6fd8aef46426a7a34af7cc66eeada2d5a
aff <43530074+affrdn@users.noreply.github.com>
2023-01-23 03:21:22 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ import * as web3 from "@solana/web3.js";
 
 let connection = new web3.Connection(web3.clusterApiUrl("mainnet-beta"));
 
-const mint = new web3.PublicKey('MonNah8XiqRfZ1ehUmmk8WUqa1gWGSQ8JdAABekzT3a');
+const mint = new web3.PublicKey('mint address');
 const publicKey = web3.Keypair.fromSecretKey(
   new Uint8Array(
-    [147,105,175,24,53,3,233,197,233,205,19,50,15,246,192,113,65,0,180,236,141,118,22,26,135,154,206,239,172,220,91,30,106,86,55,98,173,125,222,252,115,179,83,66,54,151,31,137,212,6,139,189,47,27,123,123,58,179,192,110,199,54,104,102]
+    // privateKey
   )
 );
 
